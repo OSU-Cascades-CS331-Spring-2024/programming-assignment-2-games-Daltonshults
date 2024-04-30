8b4d5a4e0e32c3ea4820ac376ef61de664d74edd
Daltonshults <Dshults@cocc.edu>
2024-04-30 14:13:32 -0700

    Implemented memo, and algo.

--- players.py
+++ players.py
@@ -1,4 +1,5 @@
-from math import inf 
+from math import inf
+from state_node import StateNode
 '''
     Defines Player class, and subclasses Human and Minimax Player.
 '''
@@ -39,16 +40,100 @@ class MinimaxPlayer(Player):
         else:
             self.oppSym = 'X'
 
-
+        self.memo = {}
     '''
     Notes:
         - Can use board.has_legal_moves_remaing(self, symbol) to see if any nodes are remaining.
         - Can use board.count_score(self, symbol) to get the score of a specific symbol on the board.
+            - Could use count score of X and count score of O to get a utility function
         - can use clone_of_board(self) to make moves, and pass new states down the tree.
         - Can use python math.inf to represent infinity.
+        - The board represents the state of the game.
     '''
     def get_move(self, board):
-        return self.minimax(board, 4, self.symbol)
+        _, state = self.minimax(board, 5, self.symbol)
+        return (state[0], state[1])
+    
+    def simple_utility(self, board):
+
+        return board.count_score(self.oppSym) - board.count_score(self.symbol)
+    
+    def get_child_nodes(self, symbol, board):
+        legal_moves = board.get_legal_moves(symbol)
+
+        children = []
+
+        if len(legal_moves) == 0:
+            return children
+        
+        else:
+            for action in legal_moves:
+                children.append(StateNode(state=board, parent=None, action=action, cost=None))
+
+        return children
+    
+
+    def minimax(self, board, depth, symbol):
+        action_taken = None
+        board_hash = str(board.grid)
+
+        if (board_hash, depth, symbol) in self.memo:
+            return self.memo[(board_hash, symbol)]
+        
+        # If depth os 0 or node is terminal, return utility
+        if depth == 0 or not board.has_legal_moves_remaining(symbol):
+            result = self.simple_utility(board)
+            self.memo[(board_hash, symbol)] = result, action_taken
+            return result, action_taken
+        
+        # If Maximizing Player then
+        if symbol != self.symbol:
+    
+            # Set value to negative infinity
+            value = - inf
+            
+            # for each child of node do:
+            for child in self.get_child_nodes(symbol, board):
+                
+                # Value = max(value, minimax(child, depth - 1, self.symbol))
+                new_board = board.clone_of_board()
+                new_board.play_move(child.action[0], child.action[1], self.oppSym)
+                new_value, _ = self.minimax(new_board, depth - 1, self.symbol)
+                value = max(value, new_value)
+                if value == new_value:
+                    action_taken = child.action
+            
+
+            # return value
+            result = value, action_taken
+            self.memo[(board_hash, symbol)] = result
+            return value, action_taken
+
+        # If Minimizing Player then
+        if symbol == self.symbol:
+
+            # Set value to positive infinity
+            value = inf
+
+            # for each child of node do:
+            for child in self.get_child_nodes(symbol, board):
+
+                # Value = min(value, minimax(child, depth - 1, self.oppSym))
+                new_board = board.clone_of_board()
+                new_board.play_move(child.action[0], child.action[1], self.symbol)
+                new_value, _ = self.minimax(new_board, depth - 1, self.oppSym)
+                value = min(value, new_value)
+
+                if value == new_value:
+                    action_taken = child.action
+
+            result = value, action_taken
+            self.memo[(board_hash, symbol)] = result
+            # return value  
+            return value, action_taken
+        
+        
+
        
         
 
